a4fe802663ba55a16e33b0a9419a0ce5be84efd4
johannpa <johann_paque@hotmail.fr>
2022-02-18 23:01:12 +0100

    Delete projets_cs/SportCoaching/SportCoaching.API directory

--- projets_cs/SportCoaching/SportCoaching.API/Models/Goal.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SportCoaching.API.Models
-{
-    public class Goal
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
