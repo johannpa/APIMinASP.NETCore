e64baa9902d255d1a60d6eca60d17c81a074b8d6
johannpa <johann_paque@hotmail.fr>
2022-02-18 22:56:02 +0100

    HttpGet only

---
+++ projets_cs/SportCoaching/SportCoaching.API/Controller/AthletesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportCoaching.API.Controller
+{
+    [ApiController]
+    [Route("api/athletes")]
+    public class AthletesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAthletes()
+        {
+            return Ok(AthletesDataStore.Current.Athletes);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetAthleteById(int id)
+        {
+            var athleteToReturn = AthletesDataStore.Current.Athletes.FirstOrDefault(a => a.Id == id);
+
+            if (athleteToReturn == null)
+            {
+               return NotFound();
+            }
+
+            return Ok(athleteToReturn);
+
+        }
+
+    }
+}
---
+++ projets_cs/SportCoaching/SportCoaching.API/Controller/GoalsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SportCoaching.API.Controller
+{
+    [ApiController]
+    [Route("api/athletes/{athleteId}/goals")]
+    public class GoalsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetGoalsByAthleteId(int athleteId)
+        {
+            var athlete = AthletesDataStore.Current.Athletes.FirstOrDefault(a => a.Id == athleteId);
+
+            if (athlete == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(athlete.Goals);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetGoalByAthleteIdAndGoalId(int athleteId, int id)
+        {
+            var athlete = AthletesDataStore.Current.Athletes.FirstOrDefault(a => a.Id == athleteId);
+
+            if (athlete == null)
+            {
+                return NotFound();
+            }
+
+
+            var goal = athlete.Goals.FirstOrDefault(g => g.Id == id);
+
+            if (goal == null)
+            {
+                return NotFound();
+            }
+            return Ok(goal);
+        }
+    }
+}
